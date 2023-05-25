e418510f79fca0b650ef7a7b7eac97eb8cb76384
andrewsmith2374 <54158695+andrewsmith2374@users.noreply.github.com>
2023-05-24 19:19:54 -0700

    Initial Commit

---
+++ Interval Timer/Interval_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Interval_TimerApp.swift
+//  Interval Timer
+//
+//  Created by Andrew Smith on 2023-05-24.
+//
+
+import SwiftUI
+
+@main
+struct Interval_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
