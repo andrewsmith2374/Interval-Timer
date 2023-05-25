72279cce3fe23fd87356df9f2fe59d340ef1fe09
andrewsmith2374 <54158695+andrewsmith2374@users.noreply.github.com>
2023-05-25 00:24:58 -0700

    Added public attributes to IntervalTimer

--- Interval Timer/Models/IntervalTimer.swift
+++ Interval Timer/Models/IntervalTimer.swift
@@ -27,18 +27,34 @@ public struct IntervalTimer {
 	 >>> intervalTimer.setIntervals([Interval(), Interval()]
 	 >>> intervalTimer.toggleAutoContinue()
 	 >>> intervalTimer.setAutoContinue(True)
+	 
+	 === Attributes ===
+	 autoContinue: Whether the timer will automatically continue to the next interval after each interval elapses
+	 id: Unique identifier
+	 intervals: A list of each interval contained in this timer
+	 title: The title of this timer
 	 */
-	
+	var autoContinue: Bool
+	let id = UUID()
+	// Change to [Interval] once Interval has been implemented
+	var intervals: Array<Any>
 	var title: String
 	
-	init(title: String = "Timer") {
+	init(autoContinue: Bool = true, intervals: Array<Any> = [], title: String = "Timer") {
 		// Creates a new IntervalTimer with given <title>, default 'Timer'
+		self.autoContinue = autoContinue
+		self.intervals = intervals
 		self.title = title
 	}
 	
 	mutating func setTitle(title: String) {
 		// Changes self.title to <title>
 		self.title = title
+	}
+	
+	func getID() -> UUID {
+		// Returns self.id
+		return self.id
 	}
 	
 	func getTitle() -> String {
