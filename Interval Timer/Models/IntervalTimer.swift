b90e606fba043691472a6deb342cac00bfdbe50b
andrewsmith2374 <54158695+andrewsmith2374@users.noreply.github.com>
2023-05-24 20:00:35 -0700

    Defined API for IntervalTimer

--- Interval Timer/Models/IntervalTimer.swift
+++ Interval Timer/Models/IntervalTimer.swift
@@ -8,6 +8,27 @@
 import Foundation
 
 public struct IntervalTimer {
+	/*
+	 Timer that continues to predetermined intervals
+	 
+	 >>> var intervalTimer = IntervalTimer()
+	 >>> print(intervalTimer)
+	 "Interval Timer with 1 Interval of 60 seconds"
+	 >>> intervalTimer.getTitle()
+	 "Timer"
+	 >>> intervalTimer.getIntervals()
+	 [Interval]
+	 >>> intervalTimer.getAutoContinue()
+	 False
+	 >>> intervalTimer.getID() is UUID
+	 True
+	 >>> intervalTimer.setTitle(title: "Test")
+	 >>> intervalTimer.addInterval()
+	 >>> intervalTimer.setIntervals([Interval(), Interval()]
+	 >>> intervalTimer.toggleAutoContinue()
+	 >>> intervalTimer.setAutoContinue(True)
+	 */
+	
 	var title: String
 	
 	init(title: String = "Timer") {
